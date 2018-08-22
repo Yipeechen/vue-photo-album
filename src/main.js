cea505c4e5718542e250df17ac42d8a0c615b8e2
Yipeechen <yipeechen0418@gmail.com>
2018-08-22 20:10:24 +0800

    add event bus

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+Vue.prototype.$bus = new Vue()  //透過此行宣告未來所有Vue實例皆可使用$bus方法，對event發送事件或監聽
 new Vue({
   el: '#app',
   router,
