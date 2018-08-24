e9d3eab35a2aacfe890c22e793372ebb1b60cf6d
Yipeechen <yipeechen0418@gmail.com>
2018-08-24 18:18:44 +0800

    Show page get data from api

--- src/components/Show.vue
+++ src/components/Show.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="show">
     <div class="photo-container">
-      <ImgDisplay/>
+      <!-- transfer(props) url to ImgDisplay -->
+      <!-- v-if : prevent Show props url to imgDisplay before server response data -->
+      <ImgDisplay :url="url" v-if="url!='' && url != null"/>
     </div>
     <div class="main-container">
       <h3 class="main-title">{{ title }}</h3>
@@ -13,6 +15,7 @@
 
 <script>
 import ImgDisplay from '@/components/ImgDisplay';
+import axios from 'axios';
 export default {
   data: function() {
     return {
@@ -24,6 +27,32 @@ export default {
   },
   components: {
     ImgDisplay: ImgDisplay
+  },
+  created() {
+    // 1) combine request url
+    const id = this.$route.params.id;
+    const showUrl = "http://35.185.111.183/api/v1/photos/" + id;
+
+    // 2) get token if user logged in (except index)
+    let params;
+    if (localStorage.getItem('photo-album-user')) {
+      const token = JSON.parse(localStorage.getItem('photo-album-user')).authToken;
+      params = { auth_token: token };
+    }
+
+    // 3) get data from api
+    axios
+      .get(showUrl, { params })
+      .then(res => {
+        this.title = res.data.title;
+        this.date = res.data.date;
+        this.description = res.data.description;
+        this.url = "http://35.185.111.183" + res.data.file_location.url;
+      })
+      .catch(err => {
+        console.error(err.response.data);
+        this.$router.push('/login');
+      });
   }
 };
 </script>
